ee3c5bd4b520e31edcb0d248198b31c3b66c8fa7
clonalejandro <clonalejandromcb@gmail.com>
2022-03-17 11:37:17 +0100

    Improve the hooks

--- src/hooks/useExperiments.js
+++ src/hooks/useExperiments.js
@@ -1,2 +1,4 @@
 import config from '../config.json'
 export default callback => config.experiments.map(callback)
+  .sort((exp1, exp2) => new Date(exp1.props.date).getTime() - new Date(exp2.props.date).getTime())
+  .reverse()
--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -1,4 +1,4 @@
-export default (repos, callback, count = 6, blackList = ['clonalejandro.github.io']) => 
+export default (repos, callback, count = 6, blackList = ['clonalejandro.github.io', 'clonalejandro']) => 
   repos.filter(e => !blackList.includes(e.name))
-              .slice(0, count)
-              .map(callback)
+    .slice(0, count)
+    .map(callback)
